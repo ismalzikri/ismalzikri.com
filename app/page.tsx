9e8b5c65964c9378f9e1ed69b5b8e77559205b4a
Laravel007 <zikriismal42@gmail.com>
2023-06-10 21:03:00 +0700

    fetch data with RSC

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+async function getData() {
+  const res = await fetch('https://my-backend-infra.mhaidarhanif.com/api')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
 
-export default function Home() {
+export default async function Home() {
+  const data = await getData()
   return (
     <main>
       <h1>Ismal Zikri</h1>
+      <pre>{JSON.stringify(data)}</pre>
     </main>
   )
 }
