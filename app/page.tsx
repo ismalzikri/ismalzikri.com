d7db0126639c6aca0dae8d9428a8d4aa363fb8d5
Laravel007 <zikriismal42@gmail.com>
2023-06-10 21:13:27 +0700

    get & show articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-async function getData() {
-  const res = await fetch('https://my-backend-infra.mhaidarhanif.com/api')
+async function getArticles() {
+  const res = await fetch(
+    "https://my-backend-infra.mhaidarhanif.com/api/articles"
+  );
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
- 
-  return res.json()
+
+  return res.json();
 }
 
 export default async function Home() {
-  const data = await getData()
+  const articles = await getArticles();
   return (
     <main>
       <h1>Ismal Zikri</h1>
-      <pre>{JSON.stringify(data)}</pre>
+      <pre>{JSON.stringify(articles, null, 2)}</pre>
     </main>
-  )
+  );
 }
