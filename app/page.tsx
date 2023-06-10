4f3e3e8156eebaac13f1d573c3deedefb115449e
Laravel007 <zikriismal42@gmail.com>
2023-06-10 21:35:50 +0700

    try sever actions

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,20 @@ async function getArticles() {
   return res.json();
 }
 
+async function addArticle() {
+  "use server";
+  console.log("article has been added");
+}
+
 export default async function Home() {
   const articles = await getArticles();
   return (
     <main>
       <h1>Ismal Zikri</h1>
       <pre>{JSON.stringify(articles, null, 2)}</pre>
+      <form action={addArticle}>
+        <button type="submit">Add article</button>
+      </form>
     </main>
   );
 }
