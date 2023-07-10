e3bf4fc17bfe8f320b16363a480dff50a4222c08
Laravel007 <zikriismal42@gmail.com>
2023-07-10 10:57:36 +0700

    feat(page.tsx): adding find me on feature & add several icons need

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+import Image from "next/image";
+
 export default async function Home() {
   return (
     <main>
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1>Ismal Zikri</h1>
-        <p>
-          <b>Software engineer, Frontend</b>
-        </p>
+        <div>
+          <h1 className="text-center">Ismal Zikri</h1>
+          <p>
+            <b>Software engineer, Frontend</b>
+          </p>
+        </div>
+        <div className="mt-3">
+          <span>Find me on</span>
+          <div className="flex items-center justify-center gap-2 mt-2">
+            <a href="https://github.com/ismalzikri" target="_blank">
+              <Image
+                src="/icons/github-icon.svg"
+                width={32}
+                height={32}
+                alt="github-icon-svg"
+              />
+            </a>
+            <a href="https://twitter.com/ZikriIsmal" target="_blank">
+              <Image
+                src="/icons/twitter-icon.gif"
+                width={25}
+                height={25}
+                alt="github-icon-svg"
+                className="rounded-full"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </main>
   );
