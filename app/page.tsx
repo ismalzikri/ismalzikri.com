80d73c5d972771c7d47b7778e2df028cfea2133e
Laravel007 <zikriismal42@gmail.com>
2023-07-10 09:30:00 +0700

    feat(page.tsx): change first landing page design with cool

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
-async function getArticles() {
-  const res = await fetch(`${process.env.API_URL}/api/articles`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-async function addArticle() {
-  "use server";
-  console.log("article has been added");
-}
-
 export default async function Home() {
-  const articles = await getArticles();
   return (
     <main>
-      <h1>Ismal Zikri</h1>
-      <pre>{JSON.stringify(articles, null, 2)}</pre>
-      <form action={addArticle}>
-        <button type="submit">Add article</button>
-      </form>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1>Ismal Zikri</h1>
+        <p>
+          <b>Software engineer, Frontend</b>
+        </p>
+      </div>
     </main>
   );
 }
