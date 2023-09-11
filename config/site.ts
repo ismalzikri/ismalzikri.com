8754a73110d2b56f998b72e23d8df65cd0067a1d
Laravel007 <zikriismal42@gmail.com>
2023-09-11 16:58:01 +0700

    update once again

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "Ismal Zikri",
   description: "My personal thoughts about engineering",
   url: "https://ismalzikri.com",
-  ogImage: "https://ismalzikri.com/opengraph-image.png",
+  ogImage: "https://ismalzikri.com/og.png",
   links: {
     twitter: "https://twitter.com/ZikriIsmal",
     github: "https://github.com/ismalzikri/ismalzikri.com",
